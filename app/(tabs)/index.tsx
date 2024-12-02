2708294dcf594b539305e6aa026817c2c9316416
Imad Niabou <141964358+ImadNiabou@users.noreply.github.com>
2024-12-02 21:45:00 +0100

    clean up & dummy data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,15 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 
 import { ScreenContent } from '~/components/ScreenContent';
 
-export default function Home() {
+export default function Events() {
   return (
     <>
       <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
+      <View>
         <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
       </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
